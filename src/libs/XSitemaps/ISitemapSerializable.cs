ee7eaa41a16df092af2b7724667076abc72f431d
Takaaki Suzuki <xin9le@live.jp>
2026-02-02 15:21:45 +0900

    Add ISitemapSerializable

---
+++ src/libs/XSitemaps/ISitemapSerializable.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Linq;
+
+namespace XSitemaps;
+
+
+
+/// <summary>
+/// Provides conversion functionality to sitemap XML.
+/// </summary>
+internal interface ISitemapSerializable
+{
+    /// <summary>
+    /// Converts to <see cref="XElement"/>.
+    /// </summary>
+    /// <returns></returns>
+    XElement ToXElement();
+}
