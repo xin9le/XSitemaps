c95a75c81b80f4004cfe52eac0e8b1bfd473c9e9
Takaaki Suzuki <xin9le@live.jp>
2026-02-03 17:31:23 +0900

    Extract the sitemap.xml namespaces into a separate file.

--- src/libs/XSitemaps/Internals/SitemapConstants.cs
+++ src/libs/XSitemaps/Internals/SitemapConstants.cs
@@ -11,28 +11,4 @@ internal static class SitemapConstants
     /// Represents maximum url count.
     /// </summary>
     public const int MaxUrlCount = 50000;
-
-
-    /// <summary>
-    /// Represents namespace for sitemap.xml.
-    /// </summary>
-    public const string XmlNamespace = "http://www.sitemaps.org/schemas/sitemap/0.9";
-
-
-    /// <summary>
-    /// Represents schema instance namespace for sitemap.xml.
-    /// </summary>
-    public const string XmlSchemaInstance = "http://www.w3.org/2001/XMLSchema-instance";
-
-
-    /// <summary>
-    /// Represents schema location namespace for sitemap.xml.
-    /// </summary>
-    public const string SitemapSchemaLocation = "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd";
-
-
-    /// <summary>
-    /// Represents schema location namespace for sitemapindex.xml.
-    /// </summary>
-    public const string SiteindexSchemaLocation = "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd";
 }
